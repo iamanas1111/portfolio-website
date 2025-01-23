979d5d8bd319eb645a5e2481210b0c0ad8ea9c4f
anas <mohdanas@nuclaysolutions.com>
2025-01-23 17:52:07 +0530

    change

--- components/ui/DownloadResume.tsx
+++ components/ui/DownloadResume.tsx
@@ -5,7 +5,7 @@ import { IoCloudDownloadOutline } from "react-icons/io5";
 const DownloadResume: React.FC = () => {
   return (
     <Link
-      href="https://drive.google.com/file/d/11iYCXPeL39ZueTZLLWnYkZcSxhiloMOM/view?usp=drive_link"
+      href="https://drive.google.com/file/d/1kWZfBiTn96ULl8ovLgZNRQhRKGETQ22-/view?usp=sharing"
       className="flex items-center justify-center px-3 py-2 bg-white/20 backdrop-blur-md text-white rounded-md border border-white/10 shadow-lg transition-transform duration-300 hover:bg-white/30 hover:scale-105 hover:cursor-pointer font-medium"
       target="_blank"
     >
